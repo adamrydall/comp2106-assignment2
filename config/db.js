76c6163c3ed48ceb3f553b54d8f510189a1d3f42
adamrydall <adamrydall@hotmail.com>
2016-03-21 17:08:17 -0400

    Initial Commit
    Setting up files.

---
+++ config/db.js
@@ -0,0 +1,12 @@
+// create public connection string
+module.exports = {
+
+    //local
+    //'url': 'mongo://localhost/test';
+    // live on mLab
+    'url': 'mongodb://adamrydall:pass123@ds064718.mlab.com:64718/adamcomp2106',
+    'githubClientId': '3a75aa9df792f59afaf2',
+    'githubClientSecret': '9e351de9d5cd047c870ee49685609b9011b418c9',
+    'githubCallbackUrl': 'http://localhost:3000/auth/github/callback'
+
+};
