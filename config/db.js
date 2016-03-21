a0ebccb8c55607858e1950ed2a0422e335556d14
adamrydall <adamrydall@hotmail.com>
2016-03-21 18:09:49 -0400

    Added Styles - Fixed nav-bar

--- config/db.js
+++ config/db.js
@@ -5,8 +5,8 @@ module.exports = {
     //'url': 'mongo://localhost/test';
     // live on mLab
     'url': 'mongodb://adamrydall:pass123@ds064718.mlab.com:64718/adamcomp2106',
-    //'githubClientId': '3a75aa9df792f59afaf2',
-    //'githubClientSecret': '9e351de9d5cd047c870ee49685609b9011b418c9',
-    //'githubCallbackUrl': 'http://localhost:3000/auth/github/callback'
+    'githubClientId': '3a75aa9df792f59afaf2',
+    'githubClientSecret': '9e351de9d5cd047c870ee49685609b9011b418c9',
+    'githubCallbackUrl': 'http://localhost:3000/auth/github/callback'
 
 };
--- routes/articles.js
+++ routes/articles.js
@@ -6,7 +6,7 @@ var Article = require('../models/article');
 var passport = require('passport');
 
 // set up the GET handler for the main articles page
-router.get('/', isLoggedIn, function(req, res, next) {
+router.get('/', function(req, res, next) {
     // use the Article model to retrieve all articles
     Article.find(function (err, articles) {
         // if we have an error
@@ -27,14 +27,14 @@ router.get('/', isLoggedIn, function(req, res, next) {
 });
 
 // GET handler for add to display a blank form
-router.get('/add', isLoggedIn, function(req, res, next) {
+router.get('/add', function(req, res, next) {
     res.render('articles/add', {
         title: 'Add a New Article'
     });
 });
 
 // POST handler for add to process the form
-router.post('/add', isLoggedIn, function(req, res, next) {
+router.post('/add', function(req, res, next) {
 
     // save a new article using our Article model and mongoose
     Article.create( {
@@ -48,7 +48,7 @@ router.post('/add', isLoggedIn, function(req, res, next) {
 });
 
 // GET handler for edit to show the populated form
-router.get('/:id', isLoggedIn, function(req, res, next) {
+router.get('/:id', function(req, res, next) {
     // create an id variable to store the id from the url
     var id = req.params.id;
 
@@ -69,7 +69,7 @@ router.get('/:id', isLoggedIn, function(req, res, next) {
 });
 
 // POST handler for edit to update the article
-router.post('/:id', isLoggedIn, function(req, res, next) {
+router.post('/:id', function(req, res, next) {
     // create an id variable to store the id from the url
     var id = req.params.id;
 
@@ -92,7 +92,7 @@ router.post('/:id', isLoggedIn, function(req, res, next) {
     });
 });
 //get handler for delete using the article id
-router.get('/delete/:id', isLoggedIn, function(req, res, next){
+router.get('/delete/:id', function(req, res, next){
     //grab the id parameter from the url
     var id = req.params.id;
     Article.remove({ _id: id }, function(err) {
@@ -107,16 +107,16 @@ router.get('/delete/:id', isLoggedIn, function(req, res, next){
     });
 });
 
-//auth check
-function isLoggedIn(req, res, next) {
-    //is the user authenticated>
-    if (req.isAuthenticated()) {
-        return next;
-    }
-    else {
-        res.redirect('/auth/login');
-    }
-}
+////auth check
+//function isLoggedIn(req, res, next) {
+//    //is the user authenticated>
+//    if (req.isAuthenticated()) {
+//        return next;
+//    }
+//    else {
+//        res.redirect('/auth/login');
+//    }
+//}
 
 // make public
 module.exports = router;
