0e484c740037fb9c7fcbe19124051b0e55b3a92d
adamrydall <adamrydall@hotmail.com>
2016-03-31 21:19:00 -0400

    Populate Public Business Page

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+var mongoose = require('mongoose');
+var Article = require('../models/article');
+
+/* GET users listing.
 router.get('/', function(req, res, next) {
 
   //res.send('respond with a resource');
@@ -13,5 +16,26 @@ router.get('/', function(req, res, next) {
                          users: usernames });
 });
 
+*/
 
+// set up the GET handler for the main articles page
+router.get('/', function(req, res, next) {
+    // use the Article model to retrieve all articles
+    Article.find(function (err, articles) {
+        // if we have an error
+        if (err) {
+            console.log(err);
+            res.end(err);
+        }
+        else {
+            // we got data back
+            // show the view and pass the data to it
+            res.render('articles/indexx', {
+
+                title: 'Articles',
+                articles: articles
+            });
+        }
+    });
+});
 module.exports = router;
